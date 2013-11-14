32d354ed5e1e7e19dd76b6d8867c8a84c07ecb78
Etienne Garnier <garnier.etienne@gmail.com>
2013-11-14 18:35:25 +0100

    set default language for testing to english

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ VDS::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Set the default language for testing to english
+  config.i18n.default_locale = :en
 end
