f32c1cdcab7e68c432b7dc0ecdb733ee8ffb6a6e
Etienne Garnier <garnier.etienne@gmail.com>
2013-11-17 15:27:59 +0100

    do not show error area when no errors are present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,15 @@ module ApplicationHelper
 
   # Display errors for the given instance
   def errors_for(instance)
-    html = '<ul class="error_messages">'
-    @publication.errors.full_messages.each do |error_message|
-      html += "<li>#{error_message}</li>"
+    if instance.errors.any?
+      html = '<ul class="error_messages">'
+      @publication.errors.full_messages.each do |error_message|
+        html += "<li>#{error_message}</li>"
+      end
+      html += '</ul>'
+      return html.html_safe
+    else
+      return nil
     end
-    html += '</ul>'
-    return html.html_safe
   end
 end
