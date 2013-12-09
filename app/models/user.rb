fb81f5ed43db72be69adee1d5c22d1363890c70a
Etienne Garnier <garnier.etienne@gmail.com>
2013-12-09 17:02:31 +0100

    add a 'logout' button in the admin layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable
+
+  # Default string representation is the User name capitalized
+  def to_s
+    username.capitalize
+  end
 end
